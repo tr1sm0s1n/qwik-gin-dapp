77b26fad9eae44f4782d13c5c5f1306a9b171690
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-20 17:00:24 +0530

    controllers&services_added

---
+++ server/controllers/controllers.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/DEMYSTIF/qwik-gin-dapp/server/interfaces"
+	"github.com/DEMYSTIF/qwik-gin-dapp/server/lib"
+	"github.com/DEMYSTIF/qwik-gin-dapp/server/services"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gin-gonic/gin"
+)
+
+func InfoController(ctx *gin.Context, client *ethclient.Client) {
+	networkID, chainID, latestBlock := services.InfoService(client)
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{"net_version": networkID, "eth_chainId": chainID, "eth_blockNumber": latestBlock})
+}
+
+func IssueController(ctx *gin.Context, client *ethclient.Client, instance *lib.Cert) {
+	var newCertificate interfaces.InputCertificate
+
+	if err := ctx.ShouldBind(&newCertificate); err != nil {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": "Bad request"})
+		return
+	}
+
+	trx, err := services.IssueService(client, instance, newCertificate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.IndentedJSON(http.StatusCreated, gin.H{
+		"issuedID": newCertificate.ID, "trx": trx,
+	})
+}
+
+func FetchController(ctx *gin.Context, instance *lib.Cert) {
+	param := ctx.Query("id")
+	id, err := strconv.ParseInt(param, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := services.FetchService(instance, id)
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": "Bad request"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{
+		"title": "Certificate DApp", "id": id, "name": result.Name, "course": result.Course, "grade": result.Grade, "date": result.Date,
+	})
+}
---
+++ server/services/services.go
@@ -0,0 +1,82 @@
+package services
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"log"
+	"math/big"
+	"os"
+
+	"github.com/DEMYSTIF/qwik-gin-dapp/server/interfaces"
+	"github.com/DEMYSTIF/qwik-gin-dapp/server/lib"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func InfoService(client *ethclient.Client) (*big.Int, *big.Int, uint64) {
+	networkID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chainID, err := client.ChainID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	latestBlock, err := client.BlockNumber(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return networkID, chainID, latestBlock
+}
+
+func IssueService(client *ethclient.Client, instance *lib.Cert, newCertificate interfaces.InputCertificate) (*types.Transaction, error) {
+	privateKey, err := crypto.HexToECDSA(os.Getenv("PRIVATE_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasLimit := uint64(300000)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, chainID, _ := InfoService(client)
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = gasLimit
+	auth.GasPrice = gasPrice
+	trx, err := instance.Issue(auth, big.NewInt(int64(newCertificate.ID)), newCertificate.Name, newCertificate.Course, newCertificate.Grade, newCertificate.Date)
+
+	return trx, err
+}
+
+func FetchService(instance *lib.Cert, id int64) (interfaces.ReturnCertificate, error) {
+	opts := bind.CallOpts{}
+	certID := big.NewInt(id)
+
+	result, err := instance.Certificates(&opts, certID)
+	return result, err
+}
