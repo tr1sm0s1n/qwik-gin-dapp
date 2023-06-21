33c3279a37edda8d38f8c0040ee4ded275135f62
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-21 16:27:09 +0530

    tailwind_added

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,14 +3,14 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
+    <div class="m-20">
+      <h1 class="text-4xl text-cyan-600 my-4">Hi 👋</h1>
+      <p class="text-lg text-blue-900">
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </p>
-    </>
+    </div>
   );
 });
 
