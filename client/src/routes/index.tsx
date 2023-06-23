f8bf1968ab7d5c7ee5241e6f3605b05c47b34b1d
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-23 23:07:48 +0530

    routes/index_updated

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,14 +3,42 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <div class="m-20">
-      <h1 class="text-4xl text-cyan-600 my-4">Hi 👋</h1>
-      <p class="text-lg text-blue-900">
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </div>
+    <>
+      <section class="my-8 text-center">
+        <h1 class="text-4xl text-gray-700 my-4">Certificate DApp</h1>
+        <p class="text-lg text-cyan-700">Verification Portal</p>
+      </section>
+      <section>
+        <form class="mx-auto flex items-center max-w-xs" action="/fetch">
+          <div class="relative w-full">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-gray-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                data-darkreader-inline-fill=""
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              name="id"
+              id="simple-search"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+              placeholder="Enter the certificate ID"
+              required
+            />
+          </div>
+        </form>
+      </section>
+    </>
   );
 });
 
