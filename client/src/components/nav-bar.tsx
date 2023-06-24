5327b6d50acd88414e2c30062bf7df8705bf883c
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-24 17:03:05 +0530

    nav-bar_updated

--- client/src/components/nav-bar.tsx
+++ client/src/components/nav-bar.tsx
@@ -1,7 +1,21 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, $, useStore } from '@builder.io/qwik';
 import '../icons/metamask.svg';
 
 export const NavBar = component$(() => {
+  const connection = useStore({
+    account: '',
+    status: false,
+  });
+
+  const connectToMetaMask = $(async () => {
+    const ethereum = window.ethereum;
+    if (ethereum) {
+      const wallet = await ethereum.request({ method: 'eth_requestAccounts' });
+      connection.account = wallet[0];
+      connection.status = true;
+    }
+  });
+
   return (
     <>
       <nav class="flex items-center justify-between flex-wrap bg-rose-800 p-6">
@@ -13,9 +27,32 @@ export const NavBar = component$(() => {
         </div>
         <div class="flex justify-end w-auto">
           <div>
-            <button class="flex items-center text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-rose-600 hover:bg-white">
-              <span>Connect</span>
-              <img class="h-6 w-6 ml-2" src="/metamask.svg" alt="metamask" />
+            <button
+              class="flex items-center text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-rose-600 hover:bg-white"
+              onClick$={connectToMetaMask}
+            >
+              {connection.status ? (
+                <>
+                  <img
+                    class="h-6 w-6 mr-2"
+                    src="/metamask.svg"
+                    alt="metamask"
+                  />
+                  <span>
+                    {connection.account.slice(0, 5)}...
+                    {connection.account.slice(-4)}
+                  </span>
+                </>
+              ) : (
+                <>
+                  <span>Connect</span>
+                  <img
+                    class="h-6 w-6 ml-2"
+                    src="/metamask.svg"
+                    alt="metamask"
+                  />
+                </>
+              )}
             </button>
           </div>
         </div>
