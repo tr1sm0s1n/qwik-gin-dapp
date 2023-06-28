ab6610baeee8b3611bdf8051d9b9f1a10d727033
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-28 22:51:00 +0530

    routes/issue_updated

--- client/src/routes/issue/index.tsx
+++ client/src/routes/issue/index.tsx
@@ -1,17 +1,133 @@
 import { component$, useContext } from '@builder.io/qwik';
-import { type DocumentHead } from '@builder.io/qwik-city';
+import { Form, type DocumentHead, routeAction$ } from '@builder.io/qwik-city';
 import { AuthContext } from '../layout';
+
+export const useIssue = routeAction$(async (candidate) => {
+  console.log(candidate);
+
+  return {
+    success: true,
+    candidate,
+  };
+});
 
 export default component$(() => {
   const authorized = useContext(AuthContext);
+  const action = useIssue();
 
   return authorized.value ? (
-    <>
-      <section class="my-8">
-        <h1 class="text-4xl text-gray-700 my-4">Issue Certificate</h1>
-        {/* <p class="text-lg text-cyan-700">Verification Portal</p> */}
-      </section>
-    </>
+    <section class="flex justify-center m-8">
+      <div class="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow">
+        <h3 class="text-2xl font-medium text-gray-900 text-center mb-4">
+          Issue Certificate
+        </h3>
+        <Form action={action}>
+          <div class="relative z-0 w-full mb-6 group">
+            <input
+              type="number"
+              min={1}
+              name="certificateID"
+              id="certificateID"
+              class="block py-2.5 px-0 w-full text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="certificateID"
+              class="peer-focus:font-medium absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Certificate ID
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-6 group">
+            <input
+              type="text"
+              name="candidateName"
+              id="candidateName"
+              class="block py-2.5 px-0 w-full text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="candidateName"
+              class="peer-focus:font-medium absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Candidate Name
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-6 group">
+            <select
+              id="course"
+              name="course"
+              class="block py-2.5 px-0 w-full text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            >
+              <option selected disabled hidden>
+                Choose a course
+              </option>
+              <option value="CBA">CBA</option>
+              <option value="DEB">DEB</option>
+              <option value="CED">CED</option>
+              <option value="WEB3">WEB3</option>
+            </select>
+            <label
+              for="course"
+              class="peer-focus:font-medium absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Course
+            </label>
+          </div>
+          <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 w-full mb-6 group">
+              <select
+                id="grade"
+                name="grade"
+                class="block py-2.5 px-0 w-full text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              >
+                <option selected disabled hidden>
+                  Choose a grade
+                </option>
+                <option value="S">S</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+              </select>
+              <label
+                for="grade"
+                class="peer-focus:font-medium absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Grade
+              </label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+              <input
+                type="date"
+                name="date"
+                id="date"
+                class="block py-2.5 px-0 w-full text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                required
+              />
+              <label
+                for="date"
+                class="peer-focus:font-medium absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Date
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            Submit
+          </button>
+        </Form>
+        {action.value?.success && (
+          <p class="text-lg text-green-700 mt-4">
+            Certificate issued successfully!
+          </p>
+        )}
+      </div>
+    </section>
   ) : null;
 });
 
