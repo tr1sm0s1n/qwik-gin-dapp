6404b04ed3d4bc83821fb63f77d16d9963e640f2
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-01 17:53:41 +0530

    routes/index_updated

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,29 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { Form, type DocumentHead, routeAction$ } from '@builder.io/qwik-city';
+
+type Certificate = {
+  id: number;
+  name: string;
+  course: string;
+  date: string;
+  grade: string;
+};
+
+export const useFetch = routeAction$(async (input) => {
+  const res = await fetch(
+    `${import.meta.env.PUBLIC_API}/fetch?id=${input.certificateID}`
+  );
+  const certData: Certificate = await res.json();
+
+  return {
+    success: res.status === 200,
+    data: certData,
+  };
+});
 
 export default component$(() => {
+  const action = useFetch();
+
   return (
     <>
       <section class="my-8 text-center">
@@ -9,7 +31,7 @@ export default component$(() => {
         <p class="text-lg text-cyan-700">Verification Portal</p>
       </section>
       <section>
-        <form class="mx-auto flex items-center max-w-xs" action="/fetch">
+        <Form class="mx-auto flex items-center max-w-xs" action={action}>
           <div class="relative w-full">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
               <svg
@@ -29,15 +51,47 @@ export default component$(() => {
             </div>
             <input
               type="text"
-              name="id"
-              id="simple-search"
+              name="certificateID"
+              id="certificateID"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
               placeholder="Enter the certificate ID"
               required
             />
           </div>
-        </form>
+        </Form>
       </section>
+      {action.value?.success && (
+        <section class="flex justify-center m-8">
+          <div class="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow">
+            <h3 class="text-2xl font-medium text-gray-900 text-center my-6">
+              Certificate of Completion
+            </h3>
+            <p class="text-gray-700 text-center italic">
+              This certificate is awarded to
+            </p>
+            <p class="text-2xl text-center my-3">{action.value?.data?.name}</p>
+            <p class="text-gray-700 text-center italic">
+              in recognition of their successful completion of
+            </p>
+            <p class="text-xl text-center my-3">{action.value?.data?.course}</p>
+            <p class="text-gray-700 text-center italic">
+              with a{' '}
+              <span class="text-red-800">{action.value?.data?.grade}</span>{' '}
+              Grade.
+            </p>
+            <div class="flex justify-between mx-4 my-4">
+              <div class="text-start">
+                <h2 class="text-lg font-semibold">Certificate ID:</h2>
+                <p class="text-gray-700">{action.value?.data?.id}</p>
+              </div>
+              <div class="text-end">
+                <h2 class="text-lg font-semibold">Date:</h2>
+                <p class="text-gray-700">{action.value?.data?.date}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </>
   );
 });
