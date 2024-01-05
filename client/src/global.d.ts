720b69e277320d904a19106d128bccc8b7df8d62
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-01-05 20:20:41 +0530

    🏷️ | updated global types

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -1,7 +1,11 @@
 declare global {
   interface Window {
     ethereum: {
-      request(args: { method: eth_requestAccounts }): Promise<string[]>;
+      request(args: { method: 'eth_requestAccounts' }): Promise<string[]>;
+      request(args: {
+        method: 'personal_sign';
+        params: [string | null, string | null];
+      }): Promise<string>;
     };
   }
 }
